6fcd8fa9c3c26fce8705b5b5ebca3b173071516f
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-10-31 12:37:15 -0400

    working on search functions

---
+++ CodeTest/CodeTest/MovieTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  MovieTableViewCell.swift
+//  CodeTest
+//
+//  Created by Mitchell Phillips on 10/31/16.
+//  Copyright © 2016 MitchellPhillips. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- CodeTest/CodeTest/ViewController.swift
+++ CodeTest/CodeTest/ViewController.swift
@@ -8,14 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var searchBar: UISearchBar!
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func iTunesSearch (term: String) {
+        // replace spaces with + symbol.
+        var iTunesTerm = term.replacingOccurrences(of: " ", with: "+", options: String.CompareOptions.caseInsensitive, range: nil)
+        // anything that is URL friendly should be escaped
+//        var escapedTerm = iTunesTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+//        var escapedTerm = iTunesTerm.stringByAddingPercentEscapesUsingEncoding(NSUTF8StringEncoding)
+//        var path = "https://itunes.apple.com/search?term=\(escapedTerm)&media=software"
+//        var url: NSURL = NSURL(string: path)
+//        var rqst: NSURLRequest = NSURLRequest(URL: url)
+//        var ctn: NSURLConnection = NSURLConnection(rqst: rqst, delegate: self, startImmediately: false)
+//        println("URL for searching iTunes API \(url)")
+//        ctn.start()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
     }
 
     override func didReceiveMemoryWarning() {
