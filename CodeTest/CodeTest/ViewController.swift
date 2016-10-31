5ac23f0ab36b1a4c92724dbeea9e488cc9a9a5b8
Mitchell Phillips <phillips.mitchell22@yahoo.com>
2016-10-31 12:01:12 -0400

    Added search bar, is functional

--- CodeTest/CodeTest/ViewController.swift
+++ CodeTest/CodeTest/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         print("viewDidLoad")
